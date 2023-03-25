cfcb2094aeede136da89d9698601103853d6443d
LAPTOP-J191SCGH\nat <nati.saver@gmail.com>
2023-03-26 05:11:13 +0800

    <feat> added registration of user

--- frontend/src/actions/userLoginActions.js
+++ frontend/src/actions/userLoginActions.js
@@ -9,19 +9,16 @@ import {
   USER_LOGIN_SUCCESS,
   USER_LOGIN_FAIL,
   USER_LOGOUT,
+
+  USER_REGISTER_REQUEST,
+  USER_REGISTER_SUCCESS,
+  USER_REGISTER_FAIL,
 } from '../constants/constants'
 //---------------------------------------
 
 // ACTION CREATORS ----------------
-//  Action creator for logging out a user
-//  action object contains type of the action
-export const logout = () => (dispatch) => {
-  localStorage.removeItem('userInfo')
-  dispatch({ type: USER_LOGOUT })
-}
-
 //  Action creator for logging in a user
-//  takes in username and passsword
+//  takes in username and password
 //  action object contains type of the action and payload
 export const login = (username, password) => async (dispatch) => {
   try {
@@ -55,7 +52,48 @@ export const login = (username, password) => async (dispatch) => {
   }
 }
 
+//  Action creator for logging out a user
+//  action object contains type of the action
+export const logout = () => (dispatch) => {
+  localStorage.removeItem('userInfo')
+  dispatch({ type: USER_LOGOUT })
+}
 
 
-  
+//  Action creator for registering a user
+//  takes in username and password
+//  action object contains type of the action and payload
+export const register = (name, username, email, password) => async (dispatch) => {
+  try {
+
+    dispatch({ type: USER_REGISTER_REQUEST })
+
+    // add config to indicate that the data being sent via HTTP request body is in JSON format
+    const config = {
+      headers: {
+          'Content-type': 'application/json'
+      }
+    }
+    
+    const { data } = await axios.post(
+      '/api/users/register', 
+      {'name': name, 'username': username, 'email': email, 'password': password },
+      config
+    )
+
+    dispatch({ type: USER_REGISTER_SUCCESS, payload: data })
+
+    localStorage.setItem('userInfo', JSON.stringify(data))
+
+    dispatch({ type: USER_LOGIN_SUCCESS, payload: data })
+
+  } catch (error) {
+    dispatch({ 
+      type: USER_REGISTER_FAIL, 
+      payload: error.response && error.response.data.message
+        ? error.response.data.message + error.detail
+        : error.message 
+    })
+  }
+}
 
--- frontend/src/reducers/userReducer.js
+++ frontend/src/reducers/userReducer.js
@@ -6,6 +6,10 @@ import {
   USER_LOGIN_SUCCESS,
   USER_LOGIN_FAIL,
   USER_LOGOUT,
+
+  USER_REGISTER_REQUEST,
+  USER_REGISTER_SUCCESS,
+  USER_REGISTER_FAIL,
 } from '../constants/constants'
 //---------------------------------------
 
@@ -17,7 +21,7 @@ const userInitialState = {
 }
 
 // REDUCERS ----------------
-// Reducer for getting a list of products
+// Reducer for logging/logging out a user
 export const userLoginReducer = (state = userInitialState, action) => {
   switch (action.type) {
     case USER_LOGIN_REQUEST:
@@ -36,4 +40,24 @@ export const userLoginReducer = (state = userInitialState, action) => {
       return state
   }
 }
+
+
+// Reducer for registering a user
+export const userRegisterReducer = (state = userInitialState, action) => {
+  switch (action.type) {
+    case USER_REGISTER_REQUEST:
+      return { loading: true }
+    case USER_REGISTER_SUCCESS:
+      // if succesful, return a payload of data with the products to the state
+      return { loading: false, userInfo: action.payload }
+      // if there is an error, return a new attribute error, passing in the response from the payload
+    case USER_REGISTER_FAIL:
+      return { loading: false, error: action.payload }
+      // if logout is successful   
+      // if none of the above, return the initial state
+    default:
+      return state
+  }
+}
+
 
