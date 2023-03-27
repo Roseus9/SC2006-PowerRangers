cc6111543c5b4c90c7169999bfad9f509f4ebde2
LAPTOP-J191SCGH\nat <nati.saver@gmail.com>
2023-03-27 16:00:39 +0800

    <refactor> added models into admin panel

--- backend/base/admin.py
+++ backend/base/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from .models import Bookmark, Product, Review
+from .models import Bookmark, Product, Review, Offer, Thread, ChatMessage
 # Register your models here.
 
 admin.site.register(Product)
 admin.site.register(Review)
 admin.site.register(Bookmark)
+admin.site.register(Offer)
+admin.site.register(Thread)
+admin.site.register(ChatMessage)
