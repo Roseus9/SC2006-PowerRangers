96f7feffc31fb3109448f4253048bcc72760fada
rt <1601500j@student.tp.edu.sg>
2023-03-27 16:51:03 +0800

    <feat> Add User Model

--- backend/base/admin.py
+++ backend/base/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Bookmark, Product, Review, Offer, Thread, ChatMessage
+from .models import Bookmark, Product, Review, Offer, Thread, ChatMessage, Profile
 # Register your models here.
 
 admin.site.register(Product)
@@ -8,3 +8,4 @@ admin.site.register(Bookmark)
 admin.site.register(Offer)
 admin.site.register(Thread)
 admin.site.register(ChatMessage)
+admin.site.register(Profile)
