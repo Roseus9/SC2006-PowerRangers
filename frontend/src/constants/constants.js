6814c9c96c57f50f6fd0aec02b33a16a8bea96d2
yanchi <yanchi97@gmail.com>
2023-04-02 04:23:48 +0800

    ignore

--- frontend/src/constants/constants.js
+++ frontend/src/constants/constants.js
@@ -24,23 +24,6 @@ export const PRODUCT_CREATE_REQUEST = "PRODUCT_CREATE_REQUEST";
 export const PRODUCT_CREATE_SUCCESS = "PRODUCT_CREATE_SUCCESS";
 export const PRODUCT_CREATE_FAIL = "PRODUCT_CREATE_FAIL";
 
-<<<<<<< HEAD
-//  constants for API call to create a new product
-export const OFFER_CREATE_REQUEST = "OFFER_CREATE_REQUEST";
-export const OFFER_CREATE_SUCCESS = "OFFER_CREATE_SUCCESS";
-export const OFFER_CREATE_FAIL = "OFFER_CREATE_FAIL";
-=======
-// constants for API call to get user profile details
-export const USER_DETAILS_REQUEST = "USER_DETAILS_REQUEST";
-export const USER_DETAILS_SUCCESS = "USER_DETAILS_SUCCESS";
-export const USER_DETAILS_FAIL = "USER_DETAILS_FAIL";
-
-// constants for API call to get user profile 
-export const USER_PROFILE_REQUEST = "USER_PROFILE_REQUEST";
-export const USER_PROFILE_SUCCESS = "USER_PROFILE_SUCCESS";
-export const USER_PROFILE_FAIL = "USER_PROFILE_FAIL";
->>>>>>> main
-
 //  constants for API call to create a new product
 export const OFFER_CREATE_REQUEST = "OFFER_CREATE_REQUEST";
 export const OFFER_CREATE_SUCCESS = "OFFER_CREATE_SUCCESS";
